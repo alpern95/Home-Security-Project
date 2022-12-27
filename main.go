9bcaa1ab5cefef7b19ed039664d3b758215caf13
root <root@alap-alpine.my.domain>
2022-12-27 08:57:30 +0100

    Premier commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	_ "embed"
+	"encoding/json"
+	"fmt"
+	"hash/fnv"
+	"io/ioutil"
+	"math"
+	"net/http"
+	"net/url"
+	"os"
+	"os/exec"
+	"runtime"
+	"strconv"
+	"strings"
+	"sync"
+	"text/template"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"gopkg.in/yaml.v2"
+)
+
+// Config stores the variables for runtime configuration.
